6370e19d32c3261ec8fd0095822f3601594ce4d6
EivorKinKest <sanchezjusegbm@gmail.com>
2026-06-09 15:08:11 -0500

    movil

--- app_movil/app/checkout.tsx
+++ app_movil/app/checkout.tsx
@@ -148,31 +148,3 @@ const styles = StyleSheet.create({
     fontSize: 16,
   }
 });
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 16,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 16,
-  },
-  link: {
-    color: '#0a84ff',
-    fontSize: 16,
-  },
-  confirmButton: {
-    backgroundColor: '#7d2181',
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    borderRadius: 10,
-    marginTop: 8,
-  },
-  confirmText: {
-    color: '#fff',
-    fontWeight: '700'
-  }
-});
